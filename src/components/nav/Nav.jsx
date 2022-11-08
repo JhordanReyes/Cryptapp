adec626feed5692eaf3110d644d917b714dd436c
JhordanReyes <reyesjhordan4@gmail.com>
2022-11-07 21:21:02 -0500

    Agregando modal mobile

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react"
 import "./style.css"
 
 const Nav = () => {
+
+    const [modal, setModal] = useState(false);
+
+    const handleModal = () => {
+        setModal(!modal);
+        const modalDiv = document.getElementById("modal");
+        modal ? modalDiv.style.display = "none"
+            : modalDiv.style.display = "block";
+    }
+
     return (
         <>
             <nav className='nav'>
@@ -8,7 +19,28 @@ const Nav = () => {
                     <i className='bx bx-credit-card-front'></i>
                     <span>Crypt</span>app
                 </p>
-                <i className='bx bx-menu'></i>
+                <i
+                    onClick={handleModal}
+                    className='bx bx-menu'></i>
+                <div id="modal">
+                    <ul>
+                        <li >
+                            <a target="_blank" href="https://www.linkedin.com/in/jhordanreyes/">
+                                <i className='bx bxl-linkedin-square' ></i> <span>Linkedin</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="https://github.com/JhordanReyes">
+                                <i className='bx bxl-github'></i> <span>GitHub</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a target="_blank" href="https://www.instagram.com/reyesjhordan4/">
+                                <i className='bx bxl-instagram' ></i> <span>Instagram</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
 
             <nav className="nav-desktop">
--- src/components/nav/flotante/Flotante.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Flotante = () => {
-  return (
-    <div>Flotante</div>
-  )
-}
-
-export default Flotante
